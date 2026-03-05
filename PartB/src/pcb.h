8089edfe13ed466bdecd82f2a830c01b2bced254
Reid Holloway <130070270+whodja@users.noreply.github.com>
2026-03-05 17:35:52 -0500

    pcb.h content with out comments

--- PartB/src/pcb.h
+++ PartB/src/pcb.h
@@ -1,1 +1,25 @@
+//Reid Holloway
+//3/5/2026
+//CSC-4210
+//pcb.h
 
+#ifndef PCB_H
+#define PCB_H
+
+#include <string>
+using namespace std;
+
+struct PCB
+{
+    string pid;
+    int arrival;   
+    int burst;
+    int remaining;
+    int priority;
+
+    string state;
+
+    int start_time;
+    int completion_time;
+};
+#endif
