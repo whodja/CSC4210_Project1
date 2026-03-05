364b09e1001800475765f002edf3f5b02a379691
Reid Holloway <130070270+whodja@users.noreply.github.com>
2026-03-05 17:55:19 -0500

    Added all comments to scheduler.h
    Added comments to clarify the purpose of the scheduling functions and algorithms.

--- PartB/src/scheduler.h
+++ PartB/src/scheduler.h
@@ -3,18 +3,24 @@
 //CSC-4210
 //scheduler.h
 
+//This header declares the scheduling functions that the program uses. Each function adds a different scheduling algorithm.
+//Algorithms used: FCFS, SRTF, Priority, Round Robin
+
 #ifndef SCHEDULER_H
 #define SCHEDULER_H
 
 #include "pcb.h"
 
+//Reads processes from file
 int readInput(PCB processes[]);
 
+//Scheduling algorithms
 void runFCFS(PCB processes[], int n);
 void runSRTF(PCB processes[], int n);
 void runPriority(PCB processes[], int n);
 void runRoundRobin(PCB processes[], int n, int quantum);
 
+//Helper functions
 void printState(int time, PCB *running, PCB *readyQueue[], int rqSize);
 
 #endif
