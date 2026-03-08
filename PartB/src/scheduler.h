4e43b6ed25b30bc0c908e42df2a1842e371afb03
Reid Holloway <130070270+whodja@users.noreply.github.com>
2026-03-08 17:37:31 -0400

    Add printStats function declaration to scheduler.h

--- PartB/src/scheduler.h
+++ PartB/src/scheduler.h
@@ -20,4 +20,6 @@ void runSRTF(PCB processes[], int n);
 void runPriority(PCB processes[], int n);
 void runRoundRobin(PCB processes[], int n, int quantum);
 
+void printStats(PCB processes[], int n);
+
 #endif
